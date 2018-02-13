676ceafaa04840f4733c080e36fd44a7db8b0877
AIH <harris@astro.umd.edu>
2018-02-12 21:31:00 -0500

    Updated VER to correct date

--- argusHardwareStructs.h
+++ argusHardwareStructs.h
@@ -12,7 +12,7 @@ AH 2014.07.01
 */
 
 // Version label
-#define VER "comap_20180211"
+#define VER "comap_20180212"
 
 // Run with hardware or standalone by commenting/uncommenting #define SIMULATE
 //#define SIMULATE
