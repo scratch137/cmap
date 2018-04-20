25aa9dea8d99d7097397ca1539d465002d36b5e1
AIH <harris@astro.umd.edu>
2018-04-20 15:18:34 -0400

    Updates: - Add CS low at end of HMC624 bitbanging to match spec sheet - Update version number - Nothing else

--- argusHardwareStructs.h
+++ argusHardwareStructs.h
@@ -12,7 +12,7 @@ AH 2014.07.01
 */
 
 // Version label
-#define VER "comap_20180325"
+#define VER "comap_20180420"
 
 // Run with hardware or standalone by commenting/uncommenting #define SIMULATE
 //#define SIMULATE
