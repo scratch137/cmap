9e2083505633ddbd0612ac49db5940a6ef7e52e7
AIH <harris@astro.umd.edu>
2023-06-02 13:29:34 -0400

    Catch up date with changes
    Previous version went to OVRO, here changing the version date to match the minor change involved in getting things running again with new NB files

--- argusHardwareStructs.h
+++ argusHardwareStructs.h
@@ -14,7 +14,7 @@ AH 2014.07.01
 // set manual flag for bias or dcm2 system
 #define FOUNDLNABIASSYS 1  // 1 for bias, 0 for DCM2
 // Version label
-#define VER "comap_20230601_b"
+#define VER "comap_20230602_b"
 
 // Run with hardware or standalone by commenting/uncommenting #define SIMULATE
 //#define SIMULATE
